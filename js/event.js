5cd14449cc08bdabc5d6cfafdea01efd75c58f78
OptimisticShaggy <58822932+OptimisticShaggy@users.noreply.github.com>
2020-11-02 17:05:21 -0500

    Update to addEventListener

--- js/event.js
+++ js/event.js
@@ -1,23 +1,29 @@
+///Link Audio Sounds to a Variable
 var clapping = document.getElementById('clap');
 var kicking = document.getElementById('kick');
 var snareing = document.getElementById('thesnare');
 var doorknock = document.getElementById('doorknock');
 
-document.body.onload = function () {
+///Link Buttons to Variables
+var butClapping = document.getElementById('clapping');
+var butKicking = document.getElementById('kicking');
+var butSnareing = document.getElementById('snareing');
+
+var bodyLoad = document.body;
+
+
+window.addEventListener('load', function () {
   doorknock.play();
-};
+})
 
+butClapping.addEventListener('click', function () {
+  clapping.play();
+});
 
-document.getElementById('clapping').onclick = function () {
-  clapping.play(); 
-};
-
-document.getElementById('kicking').onmouseover = function () {
+butKicking.addEventListener('mouseover', function () {
   kicking.play();
-};
+});
 
-
-document.getElementById('snareing').onclick = function () {
-  snareing.play(); 
-};
-
+butSnareing.addEventListener('click', function () {
+  snareing.play();
+});
